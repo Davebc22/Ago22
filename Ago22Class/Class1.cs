fe3299aaf63423a1275577bf9c1f60980d5f3cc9
Eder Barrero <142675634+Davebc22@users.noreply.github.com>
2023-08-22 21:23:48 -0500

    Ejercicio Promedio
    Ejercicio Promedio

---
+++ Ago22Class/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ago22Class
+{
+    public class Class1
+    {
+
+        public static double Promedio(double a, double b, double c) {
+            double promedio = (a + b + c) / 3;
+            return promedio;
+        }
+        
+
+
+    }
+}
--- Ago22Class/Program.cs
+++ Ago22Class/Program.cs
@@ -1,10 +1,15 @@
 ﻿namespace Ago22Class
 {
+    using System;
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine(Class1.Promedio(a, b, c)); // Llamar clase
         }
     }
 }
